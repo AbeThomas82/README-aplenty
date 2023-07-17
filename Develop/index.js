d5d337d22518d7bfbffb629e1ad4793836d20317
Sherwin Abraham <sherwinabraham@gmail.com>
2023-07-17 16:09:29 -0500

    Added questions.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What's the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a description of what your project is about."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide details on how you would install your project."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How would the end user work with your project?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please include suggestions on how to improve the project."
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please include test queries."
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function (answers){
+    console.log(answers);
+})}
 
 // Function call to initialize app
 init();
