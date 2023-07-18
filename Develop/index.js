8b22693b8efbf0564c3482a522e129f325d2237d
Sherwin Abraham <sherwinabraham@gmail.com>
2023-07-18 15:29:41 -0500

    Added starter.txt

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,9 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
+fs.readFile('./Develop/starter.txt', (err,data) => {
+    if (err) throw (err);
+    console.log(data);
+});
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [
@@ -36,7 +40,9 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {
